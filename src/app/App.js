3ccd9e748e5db7d3878fa54b438648335dd85474
MAbdurahman <mdbdrrhm@gmail.com>
2021-10-02 03:38:42 -0500

    Add temporary Photo.jsx and modify temporary index.css

--- src/app/App.js
+++ src/app/App.js
@@ -1,14 +1,22 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import Logo from './../img/logo.svg';
+import Photo from './../components/Photo';
 
 export default function App() {
-  require('dotenv').config();
-  return (
+	require('dotenv').config();
+	//**************** variables ****************//
+	const clientID = `?client_id=${process.env.REACT_APP_ACCESS_KEY}`;
+	const mainUrl = `https://api.unsplash.com/photos/`;
+	const searchUrl = `https://api.unsplash.com/search/photos/`;
+
+	//**************** functions ****************//
+
+	return (
 		<main className='app'>
 			<header className='app-header'>
 				<img src={Logo} className='app-logo' alt='logo' />
 				<h1>ReactJS Stock Photos</h1>
 			</header>
 		</main>
-  );
+	);
 }
---
+++ src/components/Photo.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export default function Photo() {
+	return (
+		<div>
+			<h2>This is Photo component</h2>
+		</div>
+	);
+}
