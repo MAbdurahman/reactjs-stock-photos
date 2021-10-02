5f71e5e7a62a6b74f296bc57bcc5791d370ec9ce
MAbdurahman <mdbdrrhm@gmail.com>
2021-10-02 04:58:48 -0500

    Remove package from package.json, update .env file

--- src/app/App.js
+++ src/app/App.js
@@ -1,15 +1,44 @@
 import React, { useState, useEffect } from 'react';
 import Logo from './../img/logo.svg';
+import { FaSearch } from 'react-icons/fa';
 import Photo from './../components/Photo';
 
 export default function App() {
-	require('dotenv').config();
+	
 	//**************** variables ****************//
+
 	const clientID = `?client_id=${process.env.REACT_APP_ACCESS_KEY}`;
 	const mainUrl = `https://api.unsplash.com/photos/`;
 	const searchUrl = `https://api.unsplash.com/search/photos/`;
 
+  const [loading, setLoading] = useState(false);
+  const [photos, setPhotos] = useState([]);
+  const [query, setQuery] = useState('')
+
+
 	//**************** functions ****************//
+	const fetchImages = async () => {
+    setLoading(true);
+		let url;
+    url = `${mainUrl}${clientID}`;
+		try {
+			const response = await fetch(url);
+      const data = await response.json();
+      setPhotos(data);
+      setLoading(false);
+		} catch (error) {
+      setLoading(false);
+			console.log(error);
+		}
+	};
+  
+  const handleSubmit = (e) => {
+    console.log('handle submit')
+  }
+
+  useEffect(() => {
+    fetchImages();
+  }, []);
 
 	return (
 		<main className='app'>
@@ -17,6 +46,24 @@ export default function App() {
 				<img src={Logo} className='app-logo' alt='logo' />
 				<h1>ReactJS Stock Photos</h1>
 			</header>
+			<section className='search'>
+				<form className='search-form'>
+					<input
+						type='text'
+						placeholder='search'
+						value={query}
+						onChange={e => setQuery(e.target.value)}
+						className='form-input'
+					/>
+					<button
+						type='submit'
+						className='submit-btn'
+						onClick={handleSubmit}
+					>
+						<FaSearch />
+					</button>
+				</form>
+			</section>
 		</main>
 	);
 }
